4304d53f7cf34506a924fd541f1cd0c9c2d8420e
kunkka0229 <654621965@qq.com>
2017-01-26 21:45:07 +0800

    es6语法修改、map、form

---
+++ pages/formTest/formTest.js
@@ -0,0 +1,10 @@
+// pages/formTest/formTest.js
+Page({
+  data:{},
+  onLoad:function(options){
+    // 页面初始化 options为页面跳转所带来的参数
+  },
+  onBindChange(event) {
+    console.log(event.detail.value)
+  }
+})
--- pages/movies/movie-detail/movie-detail.js
+++ pages/movies/movie-detail/movie-detail.js
@@ -1,5 +1,7 @@
 var app = getApp();
 var util = require('../../../utils/utils.js');
+// es6语法
+import {Movie} from 'class/Movie.js';
 Page({
   data:{
     movie: {}
@@ -8,47 +10,55 @@ Page({
     // 点击的电影Id
     var movieId = options.id;
     var url = app.globalData.doubanBase + '/v2/movie/subject/' + movieId;
-    util.http(url, this.processDoubanData);
-  },
-  processDoubanData(res) {
-    if(!res){
-      return;
-    }
-    var data = res.data;
-    // 导演
-    var director = {
-      avatar: '',
-      name: '',
-      id: ''
-    }
-    if(data.directors[0] != null){
-      if(data.directors[0].avatars != null){
-        director.avatar = data.directors[0].avatars.large;
-      }
-      director.name = data.directors[0].name;
-      director.id = data.directors[0].id; 
-    }
+    // util.http(url, this.processDoubanData);
 
-    var movie = {
-      movieImg: data.images ? data.images.large : '',
-      country: data.countries[0],
-      title: data.title,
-      originalTitle: data.original_title,
-      wishCount: data.wish_count,
-      commentCount: data.comments_count,
-      year: data.year,
-      genres: data.genres.join('、'),
-      stars: util.convertToStarsArray(data.rating.stars),
-      score: data.rating.average,
-      director: director,
-      casts: util.convertToCastString(data.casts),
-      castsInfo: util.convertToCastInfos(data.casts),
-      summary: data.summary
-    }
-    this.setData({
-      movie: movie
-    })
+    // es6写法
+    var movie = new Movie(url);
+    movie.getMovieData((movieData) => {
+       this.setData({
+        movie: movieData
+      })
+    });
   },
+  // processDoubanData(res) {
+  //   if(!res){
+  //     return;
+  //   }
+  //   var data = res.data;
+  //   // 导演
+  //   var director = {
+  //     avatar: '',
+  //     name: '',
+  //     id: ''
+  //   }
+  //   if(data.directors[0] != null){
+  //     if(data.directors[0].avatars != null){
+  //       director.avatar = data.directors[0].avatars.large;
+  //     }
+  //     director.name = data.directors[0].name;
+  //     director.id = data.directors[0].id; 
+  //   }
+
+  //   var movie = {
+  //     movieImg: data.images ? data.images.large : '',
+  //     country: data.countries[0],
+  //     title: data.title,
+  //     originalTitle: data.original_title,
+  //     wishCount: data.wish_count,
+  //     commentCount: data.comments_count,
+  //     year: data.year,
+  //     genres: data.genres.join('、'),
+  //     stars: util.convertToStarsArray(data.rating.stars),
+  //     score: data.rating.average,
+  //     director: director,
+  //     casts: util.convertToCastString(data.casts),
+  //     castsInfo: util.convertToCastInfos(data.casts),
+  //     summary: data.summary
+  //   }
+  //   this.setData({
+  //     movie: movie
+  //   })
+  // },
   // 预览图片
   viewMoviePostImg(event) {
     let src = event.currentTarget.dataset.src;
